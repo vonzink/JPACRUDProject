b20e14408f014d993614425aea9623690d9c84de
Vonzink <vonzink@gmail.com>
2025-05-02 18:18:04 -0600

    Spring boot MVC created, configured dependencies

---
+++ MSFGStaff/src/main/java/com/skilldistillery/msfg/MsfgStaffApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.msfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsfgStaffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsfgStaffApplication.class, args);
+	}
+
+}
